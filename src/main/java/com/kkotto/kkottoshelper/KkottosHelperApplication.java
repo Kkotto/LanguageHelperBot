0448c0e195fe1ff0f6a277b49115c20ae8f3a4f3
kkotto <ksen.sharupich@gmail.com>
2023-11-18 18:21:10 +0300

    bot registered
    Took 19 minutes

--- src/main/java/com/kkotto/kkottoshelper/KkottosHelperApplication.java
+++ src/main/java/com/kkotto/kkottoshelper/KkottosHelperApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KkottosHelperApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(KkottosHelperApplication.class, args);
     }
-
 }
---
+++ src/main/java/com/kkotto/kkottoshelper/bot/KkottosHelper.java
@@ -0,0 +1,18 @@
+package com.kkotto.kkottoshelper.bot;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Component
+public class KkottosHelper extends TelegramLongPollingBot {
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+}
