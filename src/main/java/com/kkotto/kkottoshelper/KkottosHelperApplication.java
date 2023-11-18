e2644ad5a0d6f24ee50d8325fafc94cf271714de
kkotto <ksen.sharupich@gmail.com>
2023-11-18 19:36:35 +0300

    bot configured
    Took 1 hour 10 minutes

--- src/main/java/com/kkotto/kkottoshelper/KkottosHelperApplication.java
+++ src/main/java/com/kkotto/kkottoshelper/KkottosHelperApplication.java
@@ -1,11 +1,20 @@
 package com.kkotto.kkottoshelper;
 
-import org.springframework.boot.SpringApplication;
+import com.kkotto.kkottoshelper.bot.KkottosHelper;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @SpringBootApplication
 public class KkottosHelperApplication {
     public static void main(String[] args) {
-        SpringApplication.run(KkottosHelperApplication.class, args);
+//        SpringApplication.run(KkottosHelperApplication.class, args);
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new KkottosHelper());
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/com/kkotto/kkottoshelper/bot/KkottosHelper.java
+++ src/main/java/com/kkotto/kkottoshelper/bot/KkottosHelper.java
@@ -1,18 +1,46 @@
 package com.kkotto.kkottoshelper.bot;
 
+import com.kkotto.kkottoshelper.gitignore.CustomConsts;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
 public class KkottosHelper extends TelegramLongPollingBot {
+    @Value("${bot.token}")
+    private String botToken;
+    @Value("${bot.username}")
+    private String botUsername;
+
+    public KkottosHelper() {
+        this.botToken = CustomConsts.BOT_TOKEN;
+        this.botUsername = CustomConsts.BOT_USERNAME;
+        System.out.println("botToken: " + this.botToken);
+        System.out.println("botUsername: " + this.botUsername);
+    }
+
     @Override
     public void onUpdateReceived(Update update) {
-
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(update.getMessage().getChatId());
+        sendMessage.setText("Hello!");
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public String getBotUsername() {
-        return null;
+        return this.botUsername;
+    }
+
+    @Override
+    public String getBotToken() {
+        return this.botToken;
     }
 }
