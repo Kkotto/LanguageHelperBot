9d026d4022ab077d1230748781a398c05cac2e0c
kkotto <ksen.sharupich@gmail.com>
2023-11-18 15:56:55 +0300

    Initial commit

---
+++ src/main/java/com/kkotto/kkottoshelper/KkottosHelperApplication.java
@@ -0,0 +1,13 @@
+package com.kkotto.kkottoshelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkottosHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KkottosHelperApplication.class, args);
+    }
+
+}
